6ea7fc107e823bc0597bfd6eed2b58906d7d473e
Henrique Alarcon <henrique.alarcon@hotmail.com>
2022-03-23 13:59:55 -0300

    feat:styles: Implementa styled components e pagina de resultados

---
+++ src/styles/result.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const TextResult = styled.h1`
+font-size: 20px;
+margin-top: 4em;
+text-align: center;
+color: #001427;
+`;
+
+export default TextResult;
